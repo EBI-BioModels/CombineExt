ec373a6acd6510e6e8fe44243cc8cb7aa982be17
Tung Nguyen <nvntung@gmail.com>
2021-11-16 16:49:58 +0000

    Apply for the auto reformat code from IntelliJ IDEA

--- src/main/java/de/unirostock/sems/cbext/ResourcesGenerator.java
+++ src/main/java/de/unirostock/sems/cbext/ResourcesGenerator.java
@@ -2,19 +2,19 @@
  * Copyright © 2014-2015:
  * - Martin Scharm <martin@binfalse.de>
  * - Martin Peters <martin@freakybytes.net>
- * 
+ * <p>
  * This file is part of the CombineExt library.
- * 
+ * <p>
  * CombineExt is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ * <p>
  * CombineExt is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with CombineExt. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,16 +22,14 @@ package de.unirostock.sems.cbext;
 
 /**
  * @author Martin Scharm
- * 
+ *
  */
-public class ResourcesGenerator
-{
-	
-	/**
-	 * @param args the arguments
-	 */
-	public static void main (String[] args)
-	{
-		// TODO
-	}
+public class ResourcesGenerator {
+
+   /**
+    * @param args the arguments
+    */
+   public static void main(String[] args) {
+      // TODO
+   }
 }
